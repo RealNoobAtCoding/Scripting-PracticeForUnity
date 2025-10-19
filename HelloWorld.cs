14745f63506f3f3c00819d1962a2b0673bebf0c7
RealNoobAtCoding <sasamiheev581@gmail.com>
2025-10-19 13:14:25 +0300

    HelloWorld.cs
    This is my first script ever made in C# that prints "Hello, World!" (quite basic)

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Вывод сообщения.
+        print("I rule this world!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
