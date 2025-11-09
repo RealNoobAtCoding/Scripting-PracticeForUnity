0acb752e997402b47b45980d36cf8a27e7e829bf
RealNoobAtCoding <sasamiheev581@gmail.com>
2025-11-09 15:29:45 +0500

    TrapFunction.cs
    This script changes current scene when player's collider enters trap's one.

---
+++ TrapFunction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class TrapFunction : MonoBehaviour
+{
+    public string sceneName = "Level_1"; // Объявляет имя сцены, а именно точку возврата.
+
+    void OnTriggerEnter(Collider other) // При коллизии -
+    {
+        EditorSceneManager.LoadScene(sceneName); // Отправляет игрока в другую сцену.
+    }
+}
