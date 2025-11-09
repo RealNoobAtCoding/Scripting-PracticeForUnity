ea2a35f1f73891ea934ead46d73d96ee140032c4
RealNoobAtCoding <sasamiheev581@gmail.com>
2025-11-09 15:17:27 +0500

    Trampoline Behaviour
    This script is responsible for giving functionality to trampolines and for changing player's jump Strength.

---
+++ TrampolineBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineBehaviour : MonoBehaviour
+{
+    public float jumpAmplifier = 2.5f; // Объявление переменной с плавающей запятой (чтобы при вводе десятичной дроби скрипт не выдавал ошибку).
+
+    void OnTriggerEnter(Collider other) // Алгоритм выполняется когда метод замечает столкновение.
+    {
+        other.GetComponent<Jump>().jumpStrength *= jumpAmplifier; // Меняет силу прыжка на произведение силы и jumpAmplifier.
+    }
+
+    void OnTriggerExit(Collider other) // Алгоритм выполняется когда метод замечает выход из коллайдера.
+    {
+        other.GetComponent<Jump>().jumpStrength /= jumpAmplifier; // Меняет силу прыжка на прежнюю.
+    }
+}
