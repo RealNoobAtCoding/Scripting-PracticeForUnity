d25625a1422252dec34acd3ce797de2ce6b3a2ec
RealNoobAtCoding <sasamiheev581@gmail.com>
2026-01-04 15:31:45 +0500

    Timer.cs
    This scripts makes UI timer work.

---
+++ Timer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public int minutes;
+    public float seconds;
+    public TextMeshProUGUI timerCounter;
+
+    // Цикл обновления составляет примерно 0.01 секунды
+    void Update()
+    {
+        seconds -= Time.deltaTime;
+
+        if (seconds <= 0)
+        {
+            if (minutes > 0)
+            {
+                seconds += 59;
+
+                minutes--;
+            }
+            else
+            {
+                // Если таймер остановился, перезагружаем текущую сцену
+                int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(sceneIndex);
+            }
+        }
+        int roundSeconds = Mathf.RoundToInt(seconds);
+        timerCounter.text = minutes + ":" + roundSeconds;
+    }
+}
